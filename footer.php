c8ef872363302b38357cae5419134afa776125fa
Fathiya Aulia <121508888+hongi1t@users.noreply.github.com>
2024-03-19 16:40:55 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <footer class="py-3 my-4">
+      <p class="text-center text-muted">Copyright © 2022 DecaVision, All Rights Reserved</p>
+    </footer>
+</div>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DecaVision</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+      header{
+        background-color:rgb(87, 187, 187);
+        border-bottom-left-radius: 24px;
+        border-bottom-right-radius: 24px;
+      }
+      @media screen and (max-width: 2000px) {
+        header {
+          padding: 30px;
+          font-size: 1.2rem;
+        }
+      }
+      @media screen and (max-width: 800px) {
+        header {
+          padding: 20px;
+          font-size: 1rem ;
+        }
+      }
+      @media screen and (max-width: 600px) {
+        header {
+          padding: 10px;
+          font-size: 0.5rem;
+        }
+      }
+    </style>
+  </head>
+ <body>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <a href="/" class="d-flex align-items-justify mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <span class="fs-4">DecaVision</span>
+          </a>
+    
+          <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Beranda</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Tentang Kami</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Kontak</a></li>
+        </header>
